a40c35cb8324491cf295a611b8891c270454ee34
KSG <hammersang@gmail.com>
2025-05-04 03:09:21 +0900

    2-7완료

--- practice/src/main/java/ko/spring/start/practice/question/QuestionController.java
+++ practice/src/main/java/ko/spring/start/practice/question/QuestionController.java
@@ -1,15 +1,27 @@
 package ko.spring.start.practice.question;
 
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
+    private final QuestionRepository questionRepository;
+
     @GetMapping("/question/list")
-    @ResponseBody
-    public String list() {
-        return "question list";
+//    @ResponseBody
+    public String list(Model model) {
+        List<Question> list = this.questionRepository.findAll();
+        System.out.println("질문 개수: " + list.size());
+        model.addAttribute("questionList", list);
+        return "question_list";
+
     }
 }
