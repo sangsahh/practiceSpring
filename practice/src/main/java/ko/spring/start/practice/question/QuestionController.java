0825bd6bdebf39aa246ce06fad4de8afd4576d22
KSG <hammersang@gmail.com>
2025-05-05 23:19:29 +0900

    2장 완료

--- practice/src/main/java/ko/spring/start/practice/question/QuestionController.java
+++ practice/src/main/java/ko/spring/start/practice/question/QuestionController.java
@@ -1,13 +1,13 @@
 package ko.spring.start.practice.question;
 
 
+import jakarta.validation.Valid;
+import ko.spring.start.practice.answer.AnswerForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +31,24 @@ public class QuestionController {
         Question question = this.questionService.getQuestion(id
         );
         model.addAttribute("question", question);
+        model.addAttribute("answerForm", new AnswerForm());
         return "question_detail";
     }
+
+    @GetMapping(value= "/create")
+    public String questionCreate(Model model) {
+        model.addAttribute("questionForm", new QuestionForm());
+        return "question_form";
+    }
+
+    @PostMapping(value = "/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list"; //질문 저장후 질문목록으로 이동
+    }
+
+
 }
