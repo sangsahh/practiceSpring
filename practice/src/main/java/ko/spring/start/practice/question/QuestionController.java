2a76a7f1354a0a28b0ecf4ae3fd2590d00cc75f6
KSG <hammersang@gmail.com>
2025-05-11 22:55:58 +0900

    3-13 검색 기능 추가하기

--- practice/src/main/java/ko/spring/start/practice/question/QuestionController.java
+++ practice/src/main/java/ko/spring/start/practice/question/QuestionController.java
@@ -30,11 +30,9 @@ public class QuestionController {
     //paging 구현
     @GetMapping("/list")
 //    @ResponseBody
-    public String list(Model model,@RequestParam(value="page", defaultValue = "0") int page,
-                       @RequestParam(value = "kw", defaultValue = "") String kw) {
-        Page<Question> paging = this.questionService.getList(page, kw);
+    public String list(Model model,@RequestParam(value="page", defaultValue = "0") int page) {
+        Page<Question> paging = this.questionService.getList(page);
         model.addAttribute("paging", paging);
-        model.addAttribute("kw", kw);
         return "question_list";
     }
 
--- practice/src/main/java/ko/spring/start/practice/question/QuestionRepository.java
+++ practice/src/main/java/ko/spring/start/practice/question/QuestionRepository.java
@@ -3,10 +3,7 @@ package ko.spring.start.practice.question;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,20 +12,4 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
     Page<Question> findAll (Pageable pageable);
-    Page<Question> findAll (Specification<Question> spec, Pageable pageable);
-
-    //자바 코드로 쿼리를 생성하는 방식보다 직접 쿼리를 작성하는게 편할때
-    @Query("select "
-            + "distinct q"
-            +"from Question q"
-            +"left outer join SiteUser u1 on q.author=u1"
-            +"left outer join Answer a on a.question=q"
-            +"left outer join SiteUser u2 on a.author=u2"
-            +"where"
-            +"      q.subject like %:kw% "
-            +"      or q.content like %:kw% "
-            +"      or u1.content like %:kw% "
-            +"      or a.content like %:kw% "
-            +"      or u2.content like %:kw% ")
-    Page<Question> findAllByKeyword(@Param("kw") String kw, Pageable pageable);
 }
